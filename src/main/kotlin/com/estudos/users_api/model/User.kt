0db6abf5e8bb09bc9d488f2f24051fc00ac2956f
Ana Julia Souza <asleite@uolinc.com>
2026-02-11 15:53:40 -0400

    feat: define User and Stack models

---
+++ src/main/kotlin/com/estudos/users_api/model/User.kt
@@ -0,0 +1,16 @@
+package com.estudos.users_api.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDate
+import java.util.UUID
+
+@Table("users")
+data class User(
+    @Id
+    val id: UUID? = null,
+
+    val name: String,
+    val nick: String?,
+    val birthDate: LocalDate
+)
